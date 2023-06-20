5e5c5c69a0c123ccf0f01c2e45db6b1091954a54
Tiyana19 <techietiyana@gmail.com>
2023-06-20 06:38:13 +0530

    project-created

--- Passenger-count-mini-project-js/script.js
+++ Passenger-count-mini-project-js/script.js
@@ -1,7 +1,19 @@
-let message = document.getElementsByClassName("person")[0];
 let count = document.getElementById("count");
 let increment = document.getElementsByClassName("increment-btn")[0];
+let save = document.getElementsByClassName("save-btn")[0];
+let entries = document.getElementsByClassName("entries")[0];
 
 increment.addEventListener("click", () => {
-  count.innerText = Number(count.innerText) + 1;
+  count.textContent = Number(count.textContent) + 1;
+});
+
+// why use textContent instead of innerText 👇
+// https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
+
+save.addEventListener("click", () => {
+  if (count.innerText == 0) {
+  } else {
+    entries.textContent = entries.textContent + " " + count.textContent + " - ";
+    count.textContent = 0;
+  }
 });
